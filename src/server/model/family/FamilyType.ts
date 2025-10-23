355d53bdee8cb7ad102cd18dcc5fd7b5b62d1c30
sifat-ur-rahman <md.sifat.ur.rahman2702@gmail.com>
2025-10-23 16:18:14 +0600

    family type & modal

---
+++ src/server/model/family/FamilyType.ts
@@ -0,0 +1,9 @@
+export interface IFamily {
+    _id: string;
+    numbering: number;
+    name: string;
+    members: number;
+    phone: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
---
+++ src/server/model/qurbani/QurbaniType.ts
@@ -0,0 +1,6 @@
+export interface IQurbani {
+    _id: string;
+
+    createdAt: Date;
+    updatedAt: Date;
+}
