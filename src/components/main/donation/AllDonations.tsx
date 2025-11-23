59717701198af22c2021e366931fb3191227b410
Md Sifat Ur Rahman <108384669+sifat-ur-rahman@users.noreply.github.com>
2025-11-23 21:40:52 +0600

    Fix animation delay in AllDonations component

--- src/components/main/donation/AllDonations.tsx
+++ src/components/main/donation/AllDonations.tsx
@@ -82,7 +82,7 @@ function AllDonationsComponent({
                         whileInView={{ opacity: 1, y: 0 }}
                         whileHover={{ scale: 1.05 }}
                         transition={{
-                            delay: index * 0.1,
+                            delay:  0.1,
                             duration: 0.5,
                             type: 'spring',
                         }}
